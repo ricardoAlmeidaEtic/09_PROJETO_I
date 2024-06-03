22bbeec4caad7523d2c59e688d64ce43d99312a7
ricardoAlmeidaEtic <ricardo_almeida@eticalgarve.com>
2024-06-03 21:03:32 +0000

    [SETUP] criação do projeto e da aplicação na framework django.

---
+++ website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+class Home(View):
+    template_name = 'home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)

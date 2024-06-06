f20c1f95163a54d11dda7b5257c6bd93ffd31a3b
ricardoAlmeidaEtic <ricardo_almeida@eticalgarve.com>
2024-06-06 20:50:53 +0000

    [UPDATE] redirect staff to admin when logged in app, added form errors on signup form, updated home ui, each item is now a web component.

--- website/static/webcomponents.js
+++ website/static/webcomponents.js
@@ -1,16 +1,64 @@
 /**TODO HEADER */
-const headerTemplate = document.createElement("template");
-headerTemplate.innerHTML = `
+const itemTemplate = document.createElement("template");
+itemTemplate.innerHTML = `
+<style>
+    .drive-item {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #ddd;
+        padding: 10px;
+        cursor: pointer;
+    }
+
+    .drive-item a{
+        display: flex;
+        align-items: center;
+        text-decoration: none; 
+        color:black;
+    }
+
+    .drive-item:hover{
+        background-color: lightgray;
+    }
+
+    .drive-item-icon {
+        width: 40px;
+        height: 40px;
+        background-color: #eee;
+        margin-right: 10px;
+    }
+
+    .drive-item-icon img{
+        width: 100%;
+    }
+
+    .drive-item-details {
+        flex: 1;
+    }
+
+    .drive-item-name {
+        font-weight: bold;
+    }
+
+    .drive-item-date {
+        color: #666;
+    }
+
+    .dive-item-delete{
+        background: red;
+    }
+</style>
+
 <li class="drive-item">
-    <a href="/website/{{folder.id}}" style="width: 95%;">
+    <a id="drive-link" style="width: 100%;">
         <div class="drive-item-icon">
-            <img src="https://cdn.iconscout.com/icon/free/png-256/free-folder-1166-470303.png">
+            <img>
         </div>
         <div class="drive-item-details">
-            <span class="drive-item-name">{{ folder.name }}</span>
-            <span class="drive-item-date">{{ folder.date }}</span>
+            <span class="drive-item-name"></span>
+            <span class="drive-item-date"></span>
         </div>
-        <a onclick="delete(' {{folder.id}} ')" style="width: 5%;">
+        <a id="delete">
             <div class="drive-item-icon dive-item-delete">
                 <img src="https://cdn.iconscout.com/icon/free/png-256/free-trash-1767922-1502175.png">
             </div>
@@ -19,33 +67,69 @@ headerTemplate.innerHTML = `
 </li>
 
 `;
+
 class Folder extends HTMLElement {
 
-    static observedAttributes = ['folder'];
+    static observedAttributes = ['id','name','type','date'];
     shadowRoot;
     #folder;
-    #folderVal;
+    #id = null;
+    #name  = null;
+    #type  = null;
+    #date  = null;
+
     constructor() {
         super();
         this.shadowRoot = this.attachShadow({mode: 'closed'});
-        this.shadowRoot.append(headerTemplate.content.cloneNode(true));
+        this.shadowRoot.appendChild(itemTemplate.content.cloneNode(true));
         this.#folder = this.shadowRoot.querySelector("#drive-item");
-        this.#folder.onclick = () => this.dispatchEvent(new CustomEvent("clicked"));
     }
 
     attributeChangedCallback(attrName, oldVal, newVal) {
-
         switch (attrName) {
-            case 'folder':
-                this.#folder = newVal
+            case 'id':
+                this.#id = newVal
+                break
+            case 'name':
+                this.#name = newVal
+                break
+            case 'type':
+                this.#type = newVal
+                break
+            case 'date':
+                this.#date = newVal
+                break
+        }
+
+        this.render()
+    }
+
+    render(){
+        const driveLink = this.shadowRoot.querySelector("#drive-link");
+        const driveItemIcon = this.shadowRoot.querySelector(".drive-item-icon");
+        const driveItemDetails = this.shadowRoot.querySelector(".drive-item-details");
+        const driveItemDelete = this.shadowRoot.querySelector("#delete");
+
+        driveLink.href=`/website/${this.#id}`
+
+        if(this.#type == 1)
+            driveItemIcon.children[0].src = "https://cdn.iconscout.com/icon/free/png-256/free-folder-1166-470303.png"
+        else
+            driveItemIcon.children[0].src = "https://cdn.iconscout.com/icon/free/png-256/free-vlc-media-player-3-735027.png"
+
+        driveItemDetails.children[0].innerText = this.#name
+        driveItemDetails.children[1].innerText = this.#date
+
+        driveItemDelete.onclick = () => this.delete(this,this.#id,this.#type);
+    }
+
+    delete(element,id,type){
+        let typeText = type === 1 ? "Folder" : "File";
+
+        if (confirm(`Are you sure you want to delete this ${typeText}?`)) {
+            element.style.display="none"
+            alert(`The ${typeText} was deleted!`);
         }
     }
-
-    get folder() {
-        return this.#folderVal;
-    }
-    set folder(val) {
-        this.#folderVal = val;
-    }
 }
-customElements.define("folder-element", Folder);
+customElements.define("new-element", Folder);
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from website.models import File, Folder
 from clonedrive.forms import FileForm, FolderForm
@@ -7,16 +7,19 @@ class Home(View):
     template_name = 'home.html'
 
     def get(self, request, folder_id=None):
+        
+        if(request.user.is_staff):
+            return redirect('/admin')
 
         context = {}
 
         try:
             if(folder_id):
-                context['files'] = File.objects.filter(folder = folder_id)
-                context['folders'] = Folder.objects.filter(parent_folder = folder_id)
+                context['files'] = File.objects.filter(folder = folder_id, user = request.user)
+                context['folders'] = Folder.objects.filter(parent_folder = folder_id, user = request.user)
             else:
-                context['files'] = File.objects.filter(folder = None)
-                context['folders'] = Folder.objects.filter(parent_folder = None)
+                context['files'] = File.objects.filter(folder = None, user = request.user)
+                context['folders'] = Folder.objects.filter(parent_folder = None, user = request.user)
         except:
             context['folders'] = None
 
